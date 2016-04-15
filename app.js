ec7ff8766bd7a3bbb239b1f2f1ba3da782168854
aileen santos <asantos3026@gmail.com>
2016-04-15 14:21:45 -0700

    added images, practiced ng-src to angular app

--- app.js
+++ app.js
@@ -8,14 +8,34 @@ app.controller('StoreController', function(){
 var gems = [
 {
     name: 'Dodecahedron',
-    price: 2.95,
+    price: 2,
     description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    images: [
+      {
+        full: 'dodecahedron-full.jpg',
+        thumbnail: 'gem-01.gif'
+      },
+      {
+        full: 'dodecahedron-full-2.jpg',
+        thumbnail: 'dodecahedron-thumb-2.gif'
+      }
+    ],
     canPurchase: true
 },
 {
     name: "Pentagonal Gem",
     price: 5.95,
     description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    images: [
+      {
+        full: 'dodecahedron-full.jpg',
+        thumbnail: 'gem-01.gif'
+      },
+      {
+        full: 'dodecahedron-full-2.jpg',
+        thumbnail: 'dodecahedron-thumb-2.gif'
+      }
+      ],
     canPurchase: true
 }
 
