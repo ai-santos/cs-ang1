99f06eb2da8d955793eb657df38c7af62a12d624
aileen santos <asantos3026@gmail.com>
2016-04-27 14:02:57 -0700

    reverted to older version with views not set to templates

--- scripts/app.js
+++ scripts/app.js
@@ -79,6 +79,20 @@ app.directive("productTabs", function(){
     controllerAs: "tab"
   };
 });
+
+    app.directive("productGallery", function() {
+      return {
+        restrict: "E",
+        templateUrl: "templates/product-gallery.html",
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: "gallery"
+      };
+    });
   
 
 })();
