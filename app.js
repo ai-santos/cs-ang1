c90681858fda3499c48d4683329b1f86949d7d46
aileen santos <asantos3026@gmail.com>
2016-04-26 17:46:07 -0700

    added Reviews controller, tabs directive, and Reviews form

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.controller('StoreController', function(){
     this.products = gems;
   });
 
+app.controller('ReviewController', function() {
+  this.review = {};
+
+  this.addReview = function(product) {
+    product.reviews.push(this.review);
+
+    this.review = {};
+  };
+});
+
 app.directive("productTabs", function(){
   return {
     controller: function() {
